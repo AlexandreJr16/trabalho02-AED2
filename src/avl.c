c595bfc0966d4ffcdbfb98d999b6431d637e54c7
Alexandre Jr <alexandrep.souzzza@gmail.com>
2026-05-01 18:52:33 -0400

    feat: add funções da AVL

--- src/avl.c
+++ src/avl.c
@@ -33,33 +33,174 @@ static NodeAVL *destroyNodeAVL(NodeAVL *node) {
   return NULL;
 }
 
-static NodeAVL *insertNode(NodeAVL *node, li chave) {}
+static int maxHeight(int a, int b) { return (a > b) ? a : b; }
 
-static NodeAVL *deleteNode(NodeAVL *node, li chave) {}
+static int heightNode(NodeAVL *node) {
+  if (node == NULL)
+    return 0;
+  return node->altura;
+}
 
-static int searchNode(NodeAVL *node, li chave) {}
+static int fatorBalanceamento(NodeAVL *node) {
+  if (node == NULL)
+    return 0;
+  return heightNode(node->esq) - heightNode(node->dir);
+}
 
-static int heightNode(NodeAVL *node) {}
+static NodeAVL *rotateRight(NodeAVL *node) {
+  NodeAVL *aux = node->esq;
+  NodeAVL *T2 = aux->dir;
 
-static int fatorBalanceamento(NodeAVL *node) {}
+  aux->dir = node;
+  node->esq = T2;
 
-static NodeAVL *balancear(NodeAVL *node) {}
+  node->altura = maxHeight(heightNode(node->esq), heightNode(node->dir)) + 1;
+  aux->altura = maxHeight(heightNode(aux->esq), heightNode(aux->dir)) + 1;
 
-static NodeAVL *minNode(NodeAVL *node) {}
+  return aux;
+}
 
-static void preOrder(NodeAVL *node) {}
+static NodeAVL *rotateLeft(NodeAVL *node) {
+  NodeAVL *aux = node->dir;
+  NodeAVL *T2 = aux->esq;
 
-static void inOrder(NodeAVL *node) {}
+  aux->esq = node;
+  node->dir = T2;
 
-static void postOrder(NodeAVL *node) {}
+  node->altura = maxHeight(heightNode(node->esq), heightNode(node->dir)) + 1;
+  aux->altura = maxHeight(heightNode(aux->esq), heightNode(aux->dir)) + 1;
 
-static NodeAVL *rotateLeft(NodeAVL *node) {}
+  return aux;
+}
 
-static NodeAVL *rotateRight(NodeAVL *node) {}
+static NodeAVL *rotateLeftRight(NodeAVL *node) {
+  node->esq = rotateLeft(node->esq);
+  return rotateRight(node);
+}
 
-static NodeAVL *rotateLeftRight(NodeAVL *node) {}
+static NodeAVL *rotateRightLeft(NodeAVL *node) {
+  node->dir = rotateRight(node->dir);
+  return rotateLeft(node);
+}
 
-static NodeAVL *rotateRightLeft(NodeAVL *node) {}
+static NodeAVL *balancear(NodeAVL *node) {
+  if (node == NULL)
+    return NULL;
+
+  node->altura = maxHeight(heightNode(node->esq), heightNode(node->dir)) + 1;
+
+  int balance = fatorBalanceamento(node);
+
+  if (balance > 1 && fatorBalanceamento(node->esq) >= 0)
+    return rotateRight(node);
+
+  if (balance > 1 && fatorBalanceamento(node->esq) < 0)
+    return rotateLeftRight(node);
+
+  if (balance < -1 && fatorBalanceamento(node->dir) <= 0)
+    return rotateLeft(node);
+
+  if (balance < -1 && fatorBalanceamento(node->dir) > 0)
+    return rotateRightLeft(node);
+
+  return node;
+}
+
+static NodeAVL *insertNode(NodeAVL *node, li chave) {
+  if (!node)
+    return createNodeAVL(chave);
+
+  if (chave < node->chave)
+    node->esq = insertNode(node->esq, chave);
+  else if (chave > node->chave)
+    node->dir = insertNode(node->dir, chave);
+  else
+    return node;
+
+  return balancear(node);
+}
+
+static NodeAVL *minNode(NodeAVL *node) {
+  if (!node) {
+    return NULL;
+  }
+  while (node->esq) {
+    node = node->esq;
+  }
+  return node;
+}
+
+static NodeAVL *deleteNode(NodeAVL *node, li chave) {
+  if (!node)
+    return node;
+
+  if (chave < node->chave)
+    node->esq = deleteNode(node->esq, chave);
+  else if (chave > node->chave)
+    node->dir = deleteNode(node->dir, chave);
+  else {
+    if (!node->esq || !node->dir) {
+      NodeAVL *temp = node->esq ? node->esq : node->dir;
+
+      if (!temp) {
+        temp = node;
+        node = NULL;
+      } else
+        *node = *temp;
+      free(temp);
+    } else {
+      NodeAVL *temp = minNode(node->dir);
+
+      node->chave = temp->chave;
+
+      node->dir = deleteNode(node->dir, temp->chave);
+    }
+  }
+
+  if (!node)
+    return node;
+
+  return balancear(node);
+}
+
+static int searchNode(NodeAVL *node, li chave) {
+  if (!node)
+    return 0;
+  while (node) {
+    if (node->chave > chave) {
+      node = node->esq;
+    } else if (node->chave < chave) {
+      node = node->dir;
+    } else {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+static void preOrder(NodeAVL *node) {
+  if (node == NULL)
+    return;
+  printf("%ld ", node->chave);
+  preOrder(node->esq);
+  preOrder(node->dir);
+}
+
+static void inOrder(NodeAVL *node) {
+  if (node == NULL)
+    return;
+  inOrder(node->esq);
+  printf("%ld ", node->chave);
+  inOrder(node->dir);
+}
+
+static void postOrder(NodeAVL *node) {
+  if (node == NULL)
+    return;
+  postOrder(node->esq);
+  postOrder(node->dir);
+  printf("%ld ", node->chave);
+}
 
 // AVL
 typedef struct AVL {
@@ -79,20 +220,26 @@ void destroyAVL(AVL *self) {
   free(self);
 }
 
-void insertAVL(AVL *self, li chave) {}
+void insertAVL(AVL *self, li chave) {
+  self->raiz = insertNode(self->raiz, chave);
+  self->tamanho++;
+}
 
-void deleteAVL(AVL *self, li chave) {}
+void deleteAVL(AVL *self, li chave) {
+  self->raiz = deleteNode(self->raiz, chave);
+  self->tamanho--;
+}
 
-int searchAVL(AVL *self, li chave) {}
+int searchAVL(AVL *self, li chave) { return searchNode(self->raiz, chave); }
 
-int heightAVL(AVL *self) {}
+int heightAVL(AVL *self) { return heightNode(self->raiz); }
 
-int sizeAVL(AVL *self) {}
+int sizeAVL(AVL *self) { return self->tamanho; }
 
-int isEmptyAVL(AVL *self) {}
+int isEmptyAVL(AVL *self) { return !self->raiz; }
 
-void preOrderAVL(AVL *self) {}
+void preOrderAVL(AVL *self) { preOrder(self->raiz); }
 
-void inOrderAVL(AVL *self) {}
+void inOrderAVL(AVL *self) { inOrder(self->raiz); }
 
-void postOrderAVL(AVL *self) {}
+void postOrderAVL(AVL *self) { postOrder(self->raiz); }
